36d8ac21bdbb1a4865121b80741624117195d020
zdietzen <diezel37@gmail.com>
2015-10-14 19:05:39 -0700

    First Commit
    Built out a bit of the API still needs work done. First push before
    going home.

---
+++ BigBank/BigBank.API/Controllers/customersController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BigBank.API;
+using BigBank.API.Models;
+
+namespace BigBank.API.Controllers
+{
+    public class customersController : ApiController
+    {
+        private BigBankEntities db = new BigBankEntities();
+
+        // GET: api/customers
+        public IQueryable<customerModel> Getcustomers()
+        {
+            return db.customers.Select(c => new customerModel
+            {
+                CustomerId = c.CustomerId,
+                FirstName = c.FirstName,
+                LastName = c. LastName,
+                CreatedDate = c.CreatedDate,
+                Address1 = c.Address1,
+                Address2 = c.Address2,
+                City = c.City,
+                State = c.State,
+                Zip = c.Zip,
+            });
+        }
+
+        // GET: api/customers/5
+        [ResponseType(typeof(customer))]
+        public IHttpActionResult Getcustomer(int id)
+        {
+            customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // PUT: api/customers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putcustomer(int id, customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!customerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/customers
+        [ResponseType(typeof(customer))]
+        public IHttpActionResult Postcustomer(customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.customers.Add(customer);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = customer.CustomerId }, customer);
+        }
+
+        // DELETE: api/customers/5
+        [ResponseType(typeof(customer))]
+        public IHttpActionResult Deletecustomer(int id)
+        {
+            customer customer = db.customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            db.customers.Remove(customer);
+            db.SaveChanges();
+
+            return Ok(customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool customerExists(int id)
+        {
+            return db.customers.Count(e => e.CustomerId == id) > 0;
+        }
+    }
+}
---
+++ BigBank/BigBank.API/Models/customerModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigBank.API.Models
+{
+    public class customerModel
+    {
+        public int CustomerId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+        public string accountsUrl
+        {
+            get
+            {
+                return "api/accounts/GetForcustomer" + CustomerId;
+            }
+        }
+    }
+}
